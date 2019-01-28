847c176a4362dbcd58189332944c170ffb0b8725
alaa <alaa.mohamed@pharos-solutions.de>
2019-01-28 04:06:15 +0200

    Login page

--- client/js/home.controller.js
+++ client/js/home.controller.js
@@ -1,78 +1,150 @@
-app.controller('authController', function ($scope, $location, $timeout, appService) {
- 
-    $scope.data = {
-        regUsername : '',
-        regPassword : '',
-        usernameAvailable : false,
-        loginUsername : '',
-        loginPassword : ''
-    };
- 
-    /* usernamme check variables starts*/
-    let TypeTimer;
-    const TypingInterval = 800;
-    /* usernamme check variables ends*/
- 
+app.controller('homeController', function ($scope, $routeParams, $location, appService){
     
-    $scope.initiateCheckUserName = () => {
-        $scope.data.usernameAvailable = false;
-        $timeout.cancel(TypeTimer);
-        TypeTimer = $timeout( () => {
-            appService.httpCall({
-                url: '/usernameCheck',
-                params: {
-                    'username': $scope.data.regUsername
+    const UserId = $routeParams.userId;
+
+    $scope.data = {
+        username: '',
+        chatlist: [],
+        selectedFriendId: null,
+        selectedFriendName: null,
+        messages: []
+    };
+
+    appService.connectSocketServer(UserId);
+
+    appService.httpCall({
+        url: '/userSessionCheck',
+        params: {
+            'userId': UserId
+        }
+    })
+    .then((response) => {
+        $scope.data.username = response.username;
+        appService.socketEmit(`chat-list`, UserId);
+        appService.socketOn('chat-list-response', (response) => {
+            $scope.$apply( () =>{
+                if (!response.error) {
+                    if (response.singleUser) {
+                        /* 
+                        * Removing duplicate user from chat list array
+                        */
+                        if ($scope.data.chatlist.length > 0) {
+                            $scope.data.chatlist = $scope.data.chatlist.filter(function (obj) {
+                                return obj.id !== response.chatList.id;
+                            });
+                        }
+                        /* 
+                        * Adding new online user into chat list array
+                        */
+                        $scope.data.chatlist.push(response.chatList);
+                    } else if (response.userDisconnected) {
+                        /* 
+                        * Removing a user from chat list, if user goes offline
+                        */
+                        $scope.data.chatlist = $scope.data.chatlist.filter(function (obj) {
+                            return obj.socketid !== response.socketId;
+                        });
+                    } else {
+                        /* 
+                        * Updating entire chatlist if user logs in
+                        */
+                        $scope.data.chatlist = response.chatList;
+                    }
+                } else {
+                    alert(`Faild to load Chat list`);
                 }
-            })
-            .then((response) => {
-                $scope.$apply( () =>{
-                    $scope.data.usernameAvailable = response.error ? true : false;
-                });
-            })
-            .catch((error) => {
-                $scope.$apply(() => {
-                    $scope.data.usernameAvailable = true;
-                });
-               
             });
-        }, TypingInterval);
-    }
- 
-    $scope.clearCheckUserName = () => {
-        $timeout.cancel(TypeTimer);
-    }
- 
-    $scope.registerUser = () => {
-        appService.httpCall({
-            url: '/registerUser',
-            params: {
-                'username': $scope.data.regUsername,
-                'password': $scope.data.regPassword
-            }
-        })
-        .then((response) => {
-            $location.path(`/login/${response.userId}`);
-            $scope.$apply();
-        })
-        .catch((error) => {
-            alert(error.message);
+        });
+
+        /*
+        * This eventt will display the new incmoing message
+        */
+        appService.socketOn('add-message-response', (response) => {
+            $scope.$apply( () => {
+                if (response && response.fromUserId == $scope.data.selectedFriendId) {
+                    $scope.data.messages.push(response);
+                    appService.scrollToBottom();
+                }
+            });
+        });       
+    })
+    .catch((error) => {
+        console.log(error.message);
+        $scope.$apply( () =>{
+            $location.path(`/`);
+        });
+    });
+
+
+    $scope.selectFriendToChat = (friendId) => {
+        /*
+        * Highlighting the selected user from the chat list
+        */
+        const friendData = $scope.data.chatlist.filter((obj) => {
+            return obj.id === friendId;
+        });
+        $scope.data.selectedFriendName = friendData[0]['username'];
+        $scope.data.selectedFriendId = friendId;
+        /**
+        * This HTTP call will fetch chat between two users
+        */
+        appService.getMessages(UserId, friendId).then( (response) => {
+            $scope.$apply(() => {
+                $scope.data.messages = response.messages;
+            });
+        }).catch( (error) => {
+            console.log(error);
+            alert('Unexpected Error, Contact your Site Admin.');
         });
     }
- 
-    $scope.loginUser = () => {
-        appService.httpCall({
-            url: '/login',
-            params: {
-                'username': $scope.data.loginUsername,
-                'password': $scope.data.loginPassword
+
+    $scope.sendMessage = (event) => {
+
+        if (event.keyCode === 13) {
+
+            let toUserId = null;
+            let toSocketId = null;
+
+            /* Fetching the selected User from the chat list starts */
+            let selectedFriendId = $scope.data.selectedFriendId;
+            if (selectedFriendId === null) {
+                return null;
             }
-        })
-        .then((response) => {
-            $location.path(`/login/${response.userId}`);
-            $scope.$apply();
-        })
-        .catch((error) => {
-            alert(error.message);
-        });
+            friendData = $scope.data.chatlist.filter((obj) => {
+                return obj.id === selectedFriendId;
+            });
+            /* Fetching the selected User from the chat list ends */
+            
+            /* Emmiting socket event to server with Message, starts */
+            if (friendData.length > 0) {
+
+                toUserId = friendData[0]['id'];
+                toSocketId = friendData[0]['socketid'];            
+
+                let messagePacket = {
+                    message: document.querySelector('#message').value,
+                    fromUserId: UserId,
+                    toUserId: toUserId,
+                    toSocketId: toSocketId
+                };
+                $scope.data.messages.push(messagePacket);
+                appService.socketEmit(`add-message`, messagePacket);
+
+                document.querySelector('#message').value = '';
+                appService.scrollToBottom();
+            }else {
+                alert('Unexpected Error Occured,Please contact Admin');
+            }
+            /* Emmiting socket event to server with Message, ends */
+        }
+    }
+
+    $scope.alignMessage = (fromUserId) => {
+        return fromUserId == UserId ? true : false;
+    }
+
+    $scope.logout = () => {
+        appService.socketEmit(`logout`, UserId);
+        $location.path(`/`);
     }
 });
